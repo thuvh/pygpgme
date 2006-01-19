ef305898d1eca95ae3877cdabab3f46831201f73
James Henstridge <james.henstridge@canonical.com>
2006-01-19 18:16:32 +0800

    Add some tests to verify that the test keys can be loaded correctly

--- gpgme/tests/__init__.py
+++ gpgme/tests/__init__.py
@@ -2,6 +2,8 @@ import unittest
 
 def test_suite():
     import gpgme.tests.test_context
+    import gpgme.tests.test_keys
     suite = unittest.TestSuite()
     suite.addTest(gpgme.tests.test_context.test_suite())
+    suite.addTest(gpgme.tests.test_keys.test_suite())
     return suite
