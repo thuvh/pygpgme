c2c5319bf6c7ec2a930c9d4210ebdb917f7f29f0
James Henstridge <james.henstridge@canonical.com>
2006-01-31 13:31:27 +0800

    Small change to test runner

--- Makefile
+++ Makefile
@@ -4,6 +4,6 @@ build:
 	$(PYTHON) setup.py build_ext -i
 
 check: build
-	$(PYTHON) test_all.py
+	$(PYTHON) test_all.py -v
 
 .PHONY: build check
--- test_all.py
+++ test_all.py
@@ -3,11 +3,8 @@ import unittest
 
 import gpgme.tests
 
-def main(argv):
-    runner = unittest.TextTestRunner(verbosity=2)
-    if not runner.run(gpgme.tests.test_suite()).wasSuccessful():
-        return 1
-    return 0
+def test_suite():
+    return gpgme.tests.test_suite()
 
 if __name__ == '__main__':
-    sys.exit(main(sys.argv))
+    unittest.main(defaultTest='test_suite')
