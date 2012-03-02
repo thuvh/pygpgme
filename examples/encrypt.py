bb0ae010204f61b3ef043b2143a71e633020b12e
James Henstridge <james@jamesh.id.au>
2012-03-02 15:24:08 +0800

    Update simple example function to work with Python 3.

--- examples/encrypt.py
+++ examples/encrypt.py
@@ -1,14 +1,17 @@
 import gpgme
-from StringIO import StringIO
+try:
+    from io import BytesIO
+except ImportError:
+    from StringIO import StringIO as BytesIO
 
 ctx = gpgme.Context()
 ctx.armor = True
 
 key = ctx.get_key('B10A449E4CFB9A60A2DB996701AF93D991CFA34D')
 
-plain = StringIO('Hello World\n')
-cipher = StringIO()
+plain = BytesIO(b'Hello World\n')
+cipher = BytesIO()
 
 ctx.encrypt([key], 0, plain, cipher)
 
-print cipher.getvalue()
+print(cipher.getvalue())
